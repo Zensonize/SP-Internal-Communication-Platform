f3f280cf8a20e3debeeeb692b03a7f5b467e5123
Sunat.P <31818562+SunatP@users.noreply.github.com>
2021-01-20 02:39:08 +0700

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -22,8 +22,10 @@ var io = require("socket.io")(http);
 var mongoose = require("mongoose");
 let users = [];
 let messages = [];
-let nameinchat = [];
-const passwd = [];
+let msg_room_2 = [];
+let msg_room_3 = [];
+let msg_room_4 = [];
+var present_room_id = "";
 const moment = require("moment");
 moment.locale("th");
 
@@ -57,6 +59,25 @@ chatModel.find((err, result) => {
   messages = result;
 });
 
+var general_room = mongoose.model("first", chatschema);
+general_room.find((err, result) => {
+  if (err) throw err;
+  msg_room_2 = result;
+  // console.log("msg from general room",messages2)
+});
+
+var private_room = mongoose.model("second", chatschema);
+private_room.find((err, result) => {
+  if (err) throw err;
+  msg_room_3 = result;
+});
+
+var emergency_room = mongoose.model("third", chatschema);
+emergency_room.find((err, result) => {
+  if (err) throw err;
+  msg_room_4 = result;
+});
+
 const userRegister = mongoose.Schema({
   registername: String,
   password: String,
@@ -69,16 +90,41 @@ userDataModel.find((err, result) => {
 });
 
 io.on("connection", (socket) => {
-  socket.emit("loggedIn", {
-    users: users.map((s) => s.username),
-    messages: messages,
-  });
+  if (present_room_id) {
+    console.log("connecting to:", present_room_id);
+  }
 
+  if (present_room_id === "Firstroom") {
+    socket.emit("loggedIn", {
+      users: users.map((s) => s.username),
+      messages: msg_room_2,
+      room: present_room_id,
+    });
+  } else if (present_room_id === "Secondroom") {
+    // console.log(msg_room_2)
+    socket.emit("loggedIn", {
+      users: users.map((s) => s.username),
+      messages2: msg_room_3,
+      room: present_room_id,
+    });
+  } else if (present_room_id === "Thirdroom") {
+    socket.emit("loggedIn", {
+      users: users.map((s) => s.username),
+      messages3: msg_room_4,
+      room: present_room_id,
+    });
+  }
   socket.on("newuser", (username) => {
     console.log(`${username} has join at the chat.`);
+
     socket.username = username;
-    users.push(socket);
-    io.emit("userOnline", socket.username);
+    if (socket) {
+      users.push(socket);
+    }
+
+    if (socket.username !== "undefined") {
+      io.emit("userOnline", socket.username);
+    }
   });
 
   socket.on("user_regis", (userdata, userpassword) => {
@@ -86,58 +132,122 @@ io.on("connection", (socket) => {
       registername: userdata,
       password: userpassword,
     });
-    console.log(`Username is ${Data.registername} Password is ${Data.password}`);
-    console.log(`Register Sucessfully!`)
-    Data.save((err,result) => {
+    console.log(
+      `Username is ${Data.registername} Password is ${Data.password}`
+    );
+    console.log(`Register Sucessfully!`);
+    Data.save((err, result) => {
       if (err) throw err;
-      io.emit("regis_success", "Register Success!")
-    })
+      io.emit("regis_success", "Register Success!");
+    });
   });
 
-  socket.on("user_auth", (user_payload, password_payload) => {
-    userDataModel.find({
-      "registername" : user_payload,
-      "password" : password_payload
-    }, (err,result) => {
-      if (err) throw err
-      // console.log(result)
-      if(result.length === 0){
-        console.log("Data not found")
-        socket.emit("auth_fail", "Username or Password is wrong")
-      }
-      else {
-        console.log("Found user data", result)
-        socket.emit("auth_success", result)
-      }
-    })
-  })
+  socket.on("user_auth", (user_payload, password_payload, room) => {
+    present_room_id = room;
+    console.log("User join to ", room);
 
-  socket.on("msg", (msg) => {
+    userDataModel.find(
+      {
+        registername: user_payload,
+        password: password_payload,
+      },
+      (err, result) => {
+        if (err) throw err;
+        // console.log(result)
+        if (result.length === 0) {
+          console.log("Data not found");
+          socket.emit("auth_fail", "Username or Password is wrong");
+        } else {
+          console.log("Found user data", result);
+          socket.emit("auth_success", result);
+        }
+      }
+    );
+  });
+
+  socket.on("msg", (msg, room) => {
+    present_room_id = room;
+    console.log(`${present_room_id} active!`);
     console.log(`${socket.username} said '${msg}'`);
 
-    let message = new chatModel({
-      username: socket.username,
-      msg: msg,
-      date: new moment().format("DD/MM/YYYY HH:mm:ss"),
-    });
-    console.log("message is ", message);
+    if (present_room_id === "Firstroom") {
+      console.log(`send msg from ${present_room_id}!`);
+      let message = new general_room({
+        username: socket.username,
+        msg: msg,
+        date: new moment().format("DD/MM/YYYY HH:mm:ss"),
+      });
+      console.log("message is ", message);
 
-    message.save((err, result) => {
-      // console.log(err, result);
-      if (err) throw err;
+      message.save((err, result) => {
+        // console.log(err, result);
+        if (err) throw err;
 
-      // console.log(err);
-      messages.push(result);
-      io.emit("msg", result);
-    });
+        // console.log(err);
+        msg_room_2.push(result);
+        io.emit("msg", result);
+      });
+    } else if (present_room_id === "Secondroom") {
+      console.log(`send msg from ${present_room_id}!`);
+      let message = new private_room({
+        username: socket.username,
+        msg: msg,
+        date: new moment().format("DD/MM/YYYY HH:mm:ss"),
+      });
+      console.log("message is ", message);
+
+      message.save((err, result) => {
+        // console.log(err, result);
+        if (err) throw err;
+
+        // console.log(err);
+        msg_room_3.push(result);
+        io.emit("msg", result);
+      });
+    } else if (present_room_id === "Thirdroom") {
+      let message = new emergency_room({
+        username: socket.username,
+        msg: msg,
+        date: new moment().format("DD/MM/YYYY HH:mm:ss"),
+      });
+      console.log("message is ", message);
+
+      message.save((err, result) => {
+        // console.log(err, result);
+        if (err) throw err;
+
+        // console.log(err);
+        msg_room_4.push(result);
+        io.emit("msg", result);
+      });
+    }
+    // let message = new chatModel({
+    //   username: socket.username,
+    //   msg: msg,
+    //   date: new moment().format("DD/MM/YYYY HH:mm:ss"),
+    // });
+    // console.log("message is ", message);
+
+    // message.save((err, result) => {
+    //   // console.log(err, result);
+    //   if (err) throw err;
+
+    //   // console.log(err);
+    //   messages.push(result);
+    //   io.emit("msg", result);
+    // });
   });
 
   // Disconnect
   socket.on("disconnect", () => {
-    
-    console.log(`${socket.username} has left the chat.`);
-    io.emit("userLeft", socket.username);
-    users.splice(users.indexOf(socket), 1);
+    if (socket.username) {
+      // throw socket.username
+      users.splice(users.indexOf(socket), 0);
+    } else {
+      console.log(`${socket.username} has left the chat.`);
+      io.emit("userLeft", socket.username);
+      users.splice(users.indexOf(socket), 1);
+    }
   });
 });
 
