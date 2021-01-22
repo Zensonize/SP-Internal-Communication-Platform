eb7e3bae146eda11f8fae09e18529f300c2873b3
krittametk <krittametk@hotmail.com>
2021-01-22 09:35:32 +0700

    add send/receive test message, network topology change functionality, free memory, clock

--- ESP32/Server-Node/src/main.cpp
+++ ESP32/Server-Node/src/main.cpp
@@ -1,4 +1,5 @@
 #include "painlessMesh.h"
+#include <Arduino_JSON.h>
 
 #define   MESH_PREFIX     "IPHC"
 #define   MESH_PASSWORD   "ThisPasswordIsHidden"
@@ -9,34 +10,70 @@ painlessMesh  mesh;
 
 // User stub
 void sendMessage() ; // Prototype so PlatformIO doesn't complain
+void sendFreeMem() ; // Prototype so PlatformIO doesn't complain
 
-Task taskSendMessage( TASK_SECOND * 1 , TASK_FOREVER, &sendMessage );
-
+Task taskSendMessage( TASK_SECOND * 5 , TASK_FOREVER, &sendMessage );
 void sendMessage() {
-  String msg = "Hi from node1";
-  msg += mesh.getNodeId();
-  mesh.sendBroadcast( msg );
-  taskSendMessage.setInterval( random( TASK_SECOND * 1, TASK_SECOND * 5 ));
+  JSONVar msg;
+  msg["FLAG"] = "DATA";
+  msg["DATA"] = "Testmessage";
+  msg["sendTime"] = String(mesh.getNodeTime());
+  mesh.sendBroadcast(JSON.stringify(msg));
+}
+
+Task taskSendFreeMem( TASK_SECOND * 10, TASK_FOREVER, &sendFreeMem);
+void sendFreeMem() {
+  JSONVar freeMem;
+  freeMem["FLAG"] = "MEM";
+  freeMem["MEM"] = String(ESP.getFreeHeap());
+  Serial.println(JSON.stringify(freeMem));
 }
 
 // Needed for painless library
 void receivedCallback( uint32_t from, String &msg ) {
-  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+  JSONVar recv = JSON.parse(msg.c_str());
+  recv["recvTime"] = String(mesh.getNodeTime());
+  recv["FROM"] = String(from);
+  Serial.println(JSON.stringify(recv));
 }
 
 void newConnectionCallback(uint32_t nodeId) {
-    Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+  JSONVar newConn;
+  newConn["FLAG"] = "NEW_CONNECTION";
+  newConn["nodeID"] = String(nodeId);
+
+  Serial.println(JSON.stringify(newConn));
 }
 
 void changedConnectionCallback() {
-  Serial.printf("Changed connections\n");
+  JSONVar changeConn;
+  changeConn["FLAG"] = "CHANGED_CONNECTION";
+
+  std::list<uint32_t> nodeList = mesh.getNodeList();
+  String nodeListStr = "";
+
+  for (std::list<uint32_t>::iterator it = nodeList.begin(); it != nodeList.end(); it++){
+    nodeListStr += String(*it);
+    nodeListStr += ",";
+  }
+
+  changeConn["NODE_LIST"] = nodeListStr;
+  changeConn["TOPOLOGY_JSON"] = mesh.subConnectionJson();
+  
+  Serial.println(JSON.stringify(changeConn));
 }
 
 void nodeTimeAdjustedCallback(int32_t offset) {
-    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+  Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+  JSONVar changeTime;
+  changeTime["FLAG"] = "CHANGED_TIME";
+  changeTime["TIME"] = String(mesh.getNodeTime());
+
+  Serial.println(JSON.stringify(changeTime));
 }
 
 void setup() {
+
   Serial.begin(115200);
 
 //mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
@@ -49,6 +86,8 @@ void setup() {
   mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
 
   userScheduler.addTask( taskSendMessage );
+  userScheduler.addTask( taskSendFreeMem );
+  taskSendFreeMem.enable();
   taskSendMessage.enable();
 }
 
