cd0231c65c4f906914b7e94fb6dc7af077eb2619
krittametk <krittametk@hotmail.com>
2021-01-30 00:44:44 +0700

    add testcase file

--- TestCase/testAsync.py
+++ TestCase/testAsync.py
@@ -1,19 +1,39 @@
-from requests import async
+import aiohttp
 import json
 import sys
 import asyncio
+import random
+from time import sleep
 
 with open('dataSet.json') as f:
-    data = json.load(f)
+    dataSet = json.load(f)
 
 msgRate = int(sys.argv[1])
 totalmsg = msgRate*5
-delayMs = int((60*1000) / msgRate)
+delaySec = int(60 / msgRate)
 
-print('running testcase with', sys.argv[1], 'msgs/min for 5 minutes')
-print('total messages', totalmsg, 'delay for each message', delayMs)
-print('dataset size:', len(data))
+print('running testcase with', msgRate, 'msgs/min for 5 minutes')
+print('total messages', totalmsg, 'delay for each message', delaySec, 'sec' , delaySec*1000, 'ms')
+print('dataset size:', len(dataSet))
+
 def getData():
+    return random.choice(dataSet)
 
+URL = 'http://127.0.0.1:5000/chat'
 
+async def send_chat(mID, data):
+    async with aiohttp.ClientSession() as session:
+        print('sending message', mID+1,'of',totalmsg, 'length', len(json.dumps(data)))
+        async with session.post(URL, 
+            headers={'Content-Type':'application/json'},
+			json=data
+        ) as response:
+            res = await response.text()
+            print(mID, res)
 
+async def main():
+    for i in range(totalmsg):
+        await send_chat(i,getData())
+        sleep(delaySec)
+
+asyncio.run(main())
