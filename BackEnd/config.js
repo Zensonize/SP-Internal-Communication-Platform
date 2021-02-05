244eb88891af853999b50382a0ba589513d18799
Sunat.P <31818562+SunatP@users.noreply.github.com>
2021-02-05 13:48:30 +0700

    Migrate code from ESP32 to Backend
    - Create new config file
    - Merged code from ESP32-Node to Backend
    -

--- BackEnd/config.js
+++ BackEnd/config.js
@@ -4,4 +4,12 @@ config.host = "http://10.5.50.206:3000";
 config.port = 3000;
 config.db = "mongodb://10.5.50.206:27017/chat"
 
+// From ESP32 
+config.SERIAL_PORT = '/dev/ttyUSB0';
+config.BACKEND_LISTEN = 'http://10.5.50.232:3000';
+config.ESP32_EMIT = '';
+config.TIMEOUT = 10000;
+config.BUADRATE = 921600;
+config.MTU = 1000;
+
 module.exports = config;
